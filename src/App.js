badecc37decd17bfb0278a4141a28493d5466876
tkoivis18 <tobias.koivisto@gmail.com>
2023-03-14 22:21:17 +0200

    added serach element to the application

--- src/App.js
+++ src/App.js
@@ -4,20 +4,36 @@ import './App.css';
 import { useEffect } from 'react';
 
 function App() {
+  const [query, setQuery] = useState('');
   const [data, setData] = useState([]);
 
-  const fetchInfo = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(
+        `https://api.github.com/search/repositories?q=${query}`
+      );
+      setData(res.data);
+    };
+    if (query.length === 0 || query.length > 2) fetchData();
+  }, [query]);
+
+  /* const fetchInfo = () => {
     return axios
-      .get('https://api.github.com/search/repositories?q=react')
+      .get('https://api.github.com/search/repositories?q=${query}')
       .then((res) => setData(res.data));
   };
 
   useEffect(() => {
     fetchInfo();
-  }, []);
+  }, []); */
 
   return (
     <div className="App">
+      <input
+        className="search"
+        placeholder="search..."
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
+      ></input>
       <table>
         <thead>
           <tr>
